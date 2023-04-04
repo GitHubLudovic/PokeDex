b002acef18bad151871eba1c06783511903560d5
GitHubLudovic <la320@proton.me>
2023-04-04 15:53:56 +0200

    quest 03

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,12 +1,32 @@
+const pokemonList = [
+    { 
+        name: "bulbasaur",
+        imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+    },
+    {
+    name: "mew",
+    }
+];
+
+
 function PokemonCard () {
+const pokemon = pokemonList[0] 
+
+
     return (
 <figure>
-    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-         alt="bulbasaur"
-    className="card-img"/>
+    {
+        pokemon.imgSrc ? 
+    <img src={pokemon.imgSrc}
+        alt = {pokemon.name}/>
+   : <p>???</p>
+    }
+    
 </figure>
     
     );
+    
 }
 
 export default PokemonCard;
+
